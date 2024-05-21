eb70a1b95d5f985bff2162eeffacdb66a2a729d5
honam <75104291+honambsn@users.noreply.github.com>
2024-05-21 21:30:19 +0700

    add: LoginForm design

---
+++ Management System/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Management_System
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.txtName = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtPass = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.btnLogin = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.checkBoxPass = new System.Windows.Forms.CheckBox();
+			this.lblClear = new System.Windows.Forms.Label();
+			this.pictureBoxClose = new System.Windows.Forms.PictureBox();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxClose)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.panel1.Controls.Add(this.pictureBoxClose);
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Controls.Add(this.pictureBox1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(320, 78);
+			this.panel1.TabIndex = 0;
+			// 
+			// txtName
+			// 
+			this.txtName.Location = new System.Drawing.Point(26, 276);
+			this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.txtName.Name = "txtName";
+			this.txtName.Size = new System.Drawing.Size(255, 23);
+			this.txtName.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(23, 243);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(75, 17);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Username:";
+			// 
+			// txtPass
+			// 
+			this.txtPass.Location = new System.Drawing.Point(26, 365);
+			this.txtPass.Margin = new System.Windows.Forms.Padding(4);
+			this.txtPass.Name = "txtPass";
+			this.txtPass.Size = new System.Drawing.Size(255, 23);
+			this.txtPass.TabIndex = 2;
+			this.txtPass.UseSystemPasswordChar = true;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(23, 332);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(73, 17);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Password:";
+			// 
+			// btnLogin
+			// 
+			this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.btnLogin.FlatAppearance.BorderSize = 0;
+			this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnLogin.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.btnLogin.ForeColor = System.Drawing.Color.White;
+			this.btnLogin.Location = new System.Drawing.Point(26, 429);
+			this.btnLogin.Name = "btnLogin";
+			this.btnLogin.Size = new System.Drawing.Size(255, 32);
+			this.btnLogin.TabIndex = 4;
+			this.btnLogin.Text = "Login";
+			this.btnLogin.UseVisualStyleBackColor = false;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(40, 40);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 5;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(100, 102);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(112, 76);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox2.TabIndex = 6;
+			this.pictureBox2.TabStop = false;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.Color.White;
+			this.label3.Location = new System.Drawing.Point(69, 35);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(172, 18);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "MANAGEMENT SYSTEM";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.BackColor = System.Drawing.Color.White;
+			this.label4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.label4.Location = new System.Drawing.Point(97, 193);
+			this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(117, 16);
+			this.label4.TabIndex = 7;
+			this.label4.Text = "Please Login First";
+			// 
+			// checkBoxPass
+			// 
+			this.checkBoxPass.AutoSize = true;
+			this.checkBoxPass.Location = new System.Drawing.Point(153, 480);
+			this.checkBoxPass.Name = "checkBoxPass";
+			this.checkBoxPass.Size = new System.Drawing.Size(128, 21);
+			this.checkBoxPass.TabIndex = 8;
+			this.checkBoxPass.Text = "Show password";
+			this.checkBoxPass.UseVisualStyleBackColor = true;
+			this.checkBoxPass.CheckedChanged += new System.EventHandler(this.checkBoxPass_CheckedChanged);
+			// 
+			// lblClear
+			// 
+			this.lblClear.AutoSize = true;
+			this.lblClear.ForeColor = System.Drawing.Color.Red;
+			this.lblClear.Location = new System.Drawing.Point(23, 481);
+			this.lblClear.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.lblClear.Name = "lblClear";
+			this.lblClear.Size = new System.Drawing.Size(62, 17);
+			this.lblClear.TabIndex = 9;
+			this.lblClear.Text = "Clear all";
+			this.lblClear.Click += new System.EventHandler(this.lblClear_Click);
+			// 
+			// pictureBoxClose
+			// 
+			this.pictureBoxClose.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxClose.Image")));
+			this.pictureBoxClose.Location = new System.Drawing.Point(295, 0);
+			this.pictureBoxClose.Name = "pictureBoxClose";
+			this.pictureBoxClose.Size = new System.Drawing.Size(25, 22);
+			this.pictureBoxClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBoxClose.TabIndex = 8;
+			this.pictureBoxClose.TabStop = false;
+			this.pictureBoxClose.Click += new System.EventHandler(this.pictureBoxClose_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(320, 550);
+			this.Controls.Add(this.lblClear);
+			this.Controls.Add(this.checkBoxPass);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.btnLogin);
+			this.Controls.Add(this.txtPass);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txtName);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.panel1);
+			this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Form1";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxClose)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.TextBox txtName;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txtPass;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button btnLogin;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.CheckBox checkBoxPass;
+		private System.Windows.Forms.Label lblClear;
+		private System.Windows.Forms.PictureBox pictureBoxClose;
+	}
+}
+
---
+++ Management System/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Management_System
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void checkBoxPass_CheckedChanged(object sender, EventArgs e)
+		{
+			if(checkBoxPass.Checked== false)
+			{
+				txtPass.UseSystemPasswordChar = true;
+
+			}
+			else
+			{
+				txtPass.UseSystemPasswordChar = false;
+			}
+		}
+
+		private void lblClear_Click(object sender, EventArgs e)
+		{
+			txtPass.Clear();
+			txtName.Clear();
+		}
+
+		private void pictureBoxClose_Click(object sender, EventArgs e)
+		{
+			if(MessageBox.Show("Exit application", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question)== DialogResult.Yes)
+			{
+				Application.Exit();
+			}
+		}
+	}
+}
