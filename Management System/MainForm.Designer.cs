f6ff77cfea6a208385f8733cc2dad37350ff4102
honam <75104291+honambsn@users.noreply.github.com>
2024-05-21 22:07:42 +0700

    add: create mainform design

---
+++ Management System/MainForm.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Management_System
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label4 = new System.Windows.Forms.Label();
+			this.customerButton5 = new Management_System.CustomerButton();
+			this.customerButton4 = new Management_System.CustomerButton();
+			this.customerButton3 = new Management_System.CustomerButton();
+			this.customerButton2 = new Management_System.CustomerButton();
+			this.customerButton1 = new Management_System.CustomerButton();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.panelMain = new System.Windows.Forms.Panel();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.customerButton5)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.customerButton4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.customerButton3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.customerButton2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.customerButton1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.White;
+			this.panel1.Controls.Add(this.label8);
+			this.panel1.Controls.Add(this.label7);
+			this.panel1.Controls.Add(this.label6);
+			this.panel1.Controls.Add(this.label5);
+			this.panel1.Controls.Add(this.label4);
+			this.panel1.Controls.Add(this.customerButton5);
+			this.panel1.Controls.Add(this.customerButton4);
+			this.panel1.Controls.Add(this.customerButton3);
+			this.panel1.Controls.Add(this.customerButton2);
+			this.panel1.Controls.Add(this.customerButton1);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Margin = new System.Windows.Forms.Padding(4);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(984, 115);
+			this.panel1.TabIndex = 0;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.label4.Location = new System.Drawing.Point(287, 81);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(87, 21);
+			this.label4.TabIndex = 15;
+			this.label4.Text = "PRODUCT";
+			// 
+			// customerButton5
+			// 
+			this.customerButton5.Image = ((System.Drawing.Image)(resources.GetObject("customerButton5.Image")));
+			this.customerButton5.ImageHover = ((System.Drawing.Image)(resources.GetObject("customerButton5.ImageHover")));
+			this.customerButton5.ImageNormal = ((System.Drawing.Image)(resources.GetObject("customerButton5.ImageNormal")));
+			this.customerButton5.Location = new System.Drawing.Point(806, 38);
+			this.customerButton5.Name = "customerButton5";
+			this.customerButton5.Size = new System.Drawing.Size(39, 36);
+			this.customerButton5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.customerButton5.TabIndex = 14;
+			this.customerButton5.TabStop = false;
+			// 
+			// customerButton4
+			// 
+			this.customerButton4.Image = ((System.Drawing.Image)(resources.GetObject("customerButton4.Image")));
+			this.customerButton4.ImageHover = ((System.Drawing.Image)(resources.GetObject("customerButton4.ImageHover")));
+			this.customerButton4.ImageNormal = ((System.Drawing.Image)(resources.GetObject("customerButton4.ImageNormal")));
+			this.customerButton4.Location = new System.Drawing.Point(707, 42);
+			this.customerButton4.Name = "customerButton4";
+			this.customerButton4.Size = new System.Drawing.Size(39, 36);
+			this.customerButton4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.customerButton4.TabIndex = 13;
+			this.customerButton4.TabStop = false;
+			// 
+			// customerButton3
+			// 
+			this.customerButton3.Image = ((System.Drawing.Image)(resources.GetObject("customerButton3.Image")));
+			this.customerButton3.ImageHover = ((System.Drawing.Image)(resources.GetObject("customerButton3.ImageHover")));
+			this.customerButton3.ImageNormal = ((System.Drawing.Image)(resources.GetObject("customerButton3.ImageNormal")));
+			this.customerButton3.Location = new System.Drawing.Point(587, 42);
+			this.customerButton3.Name = "customerButton3";
+			this.customerButton3.Size = new System.Drawing.Size(39, 36);
+			this.customerButton3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.customerButton3.TabIndex = 12;
+			this.customerButton3.TabStop = false;
+			// 
+			// customerButton2
+			// 
+			this.customerButton2.Image = ((System.Drawing.Image)(resources.GetObject("customerButton2.Image")));
+			this.customerButton2.ImageHover = ((System.Drawing.Image)(resources.GetObject("customerButton2.ImageHover")));
+			this.customerButton2.ImageNormal = ((System.Drawing.Image)(resources.GetObject("customerButton2.ImageNormal")));
+			this.customerButton2.Location = new System.Drawing.Point(443, 42);
+			this.customerButton2.Name = "customerButton2";
+			this.customerButton2.Size = new System.Drawing.Size(39, 36);
+			this.customerButton2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.customerButton2.TabIndex = 11;
+			this.customerButton2.TabStop = false;
+			// 
+			// customerButton1
+			// 
+			this.customerButton1.Image = ((System.Drawing.Image)(resources.GetObject("customerButton1.Image")));
+			this.customerButton1.ImageHover = ((System.Drawing.Image)(resources.GetObject("customerButton1.ImageHover")));
+			this.customerButton1.ImageNormal = ((System.Drawing.Image)(resources.GetObject("customerButton1.ImageNormal")));
+			this.customerButton1.Location = new System.Drawing.Point(311, 38);
+			this.customerButton1.Name = "customerButton1";
+			this.customerButton1.Size = new System.Drawing.Size(39, 36);
+			this.customerButton1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.customerButton1.TabIndex = 0;
+			this.customerButton1.TabStop = false;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.label2.Location = new System.Drawing.Point(77, 18);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(90, 32);
+			this.label2.TabIndex = 10;
+			this.label2.Text = "demy";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Goudy Stout", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(85, 51);
+			this.label1.TabIndex = 9;
+			this.label1.Text = "M";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.label3.Location = new System.Drawing.Point(18, 71);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(172, 18);
+			this.label3.TabIndex = 8;
+			this.label3.Text = "MANAGEMENT SYSTEM";
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 584);
+			this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(984, 27);
+			this.flowLayoutPanel1.TabIndex = 1;
+			// 
+			// panelMain
+			// 
+			this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panelMain.Location = new System.Drawing.Point(0, 115);
+			this.panelMain.Margin = new System.Windows.Forms.Padding(4);
+			this.panelMain.Name = "panelMain";
+			this.panelMain.Size = new System.Drawing.Size(984, 469);
+			this.panelMain.TabIndex = 2;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.label5.Location = new System.Drawing.Point(413, 81);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(98, 21);
+			this.label5.TabIndex = 16;
+			this.label5.Text = "CUSTOMER";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.label6.Location = new System.Drawing.Point(550, 81);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(113, 21);
+			this.label6.TabIndex = 17;
+			this.label6.Text = "CATEGORIES";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.label7.Location = new System.Drawing.Point(702, 81);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(48, 21);
+			this.label7.TabIndex = 18;
+			this.label7.Text = "USER";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.label8.Location = new System.Drawing.Point(789, 81);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(73, 21);
+			this.label8.TabIndex = 19;
+			this.label8.Text = "ORDERS";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(984, 611);
+			this.Controls.Add(this.panelMain);
+			this.Controls.Add(this.flowLayoutPanel1);
+			this.Controls.Add(this.panel1);
+			this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "MainForm";
+			this.Text = "MainForm";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.customerButton5)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.customerButton4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.customerButton3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.customerButton2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.customerButton1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+		private System.Windows.Forms.Panel panelMain;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private CustomerButton customerButton1;
+		private System.Windows.Forms.Label label4;
+		private CustomerButton customerButton5;
+		private CustomerButton customerButton4;
+		private CustomerButton customerButton3;
+		private CustomerButton customerButton2;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label5;
+	}
+}
