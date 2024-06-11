7f52824a0110cb426879238f6eac4f2cbe2c0d53
honam <75104291+honambsn@users.noreply.github.com>
2024-06-11 22:59:40 +0700

    change main form icon, create a product form desgin, create product module form design

---
+++ Management System/ProductForm.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Management_System
+{
+	partial class ProductForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProductForm));
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.btnAdd = new Management_System.CustomerButton();
+			this.label1 = new System.Windows.Forms.Label();
+			this.dgvUser = new System.Windows.Forms.DataGridView();
+			this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Edit = new System.Windows.Forms.DataGridViewImageColumn();
+			this.Delete = new System.Windows.Forms.DataGridViewImageColumn();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.btnAdd)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dgvUser)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(71)))), ((int)(((byte)(160)))));
+			this.panel1.Controls.Add(this.btnAdd);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panel1.Location = new System.Drawing.Point(0, 507);
+			this.panel1.Margin = new System.Windows.Forms.Padding(4);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(1129, 55);
+			this.panel1.TabIndex = 2;
+			// 
+			// btnAdd
+			// 
+			this.btnAdd.Image = ((System.Drawing.Image)(resources.GetObject("btnAdd.Image")));
+			this.btnAdd.ImageHover = ((System.Drawing.Image)(resources.GetObject("btnAdd.ImageHover")));
+			this.btnAdd.ImageNormal = ((System.Drawing.Image)(resources.GetObject("btnAdd.ImageNormal")));
+			this.btnAdd.Location = new System.Drawing.Point(1079, 4);
+			this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
+			this.btnAdd.Name = "btnAdd";
+			this.btnAdd.Size = new System.Drawing.Size(46, 47);
+			this.btnAdd.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.btnAdd.TabIndex = 12;
+			this.btnAdd.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(14, 17);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(139, 19);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Manage Product";
+			// 
+			// dgvUser
+			// 
+			this.dgvUser.AllowUserToAddRows = false;
+			this.dgvUser.BackgroundColor = System.Drawing.Color.White;
+			dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(70)))), ((int)(((byte)(160)))));
+			dataGridViewCellStyle1.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+			dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+			dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+			dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+			this.dgvUser.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+			this.dgvUser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+			this.dgvUser.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column5,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column6,
+            this.Edit,
+            this.Delete});
+			dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+			dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+			dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+			dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+			dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+			this.dgvUser.DefaultCellStyle = dataGridViewCellStyle2;
+			this.dgvUser.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dgvUser.EnableHeadersVisualStyles = false;
+			this.dgvUser.Location = new System.Drawing.Point(0, 0);
+			this.dgvUser.Margin = new System.Windows.Forms.Padding(4);
+			this.dgvUser.Name = "dgvUser";
+			this.dgvUser.Size = new System.Drawing.Size(1129, 562);
+			this.dgvUser.TabIndex = 3;
+			// 
+			// Column5
+			// 
+			this.Column5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+			this.Column5.HeaderText = "No";
+			this.Column5.Name = "Column5";
+			this.Column5.Width = 50;
+			// 
+			// Column1
+			// 
+			this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+			this.Column1.HeaderText = "Product ID";
+			this.Column1.Name = "Column1";
+			this.Column1.Width = 95;
+			// 
+			// Column2
+			// 
+			this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+			this.Column2.HeaderText = "Name";
+			this.Column2.Name = "Column2";
+			this.Column2.Width = 69;
+			// 
+			// Column3
+			// 
+			this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+			this.Column3.HeaderText = "Qty";
+			this.Column3.Name = "Column3";
+			this.Column3.Width = 54;
+			// 
+			// Column4
+			// 
+			this.Column4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+			this.Column4.HeaderText = "Price";
+			this.Column4.Name = "Column4";
+			this.Column4.Width = 63;
+			// 
+			// Column6
+			// 
+			this.Column6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+			this.Column6.HeaderText = "Description";
+			this.Column6.Name = "Column6";
+			// 
+			// Edit
+			// 
+			this.Edit.HeaderText = "";
+			this.Edit.Image = ((System.Drawing.Image)(resources.GetObject("Edit.Image")));
+			this.Edit.Name = "Edit";
+			// 
+			// Delete
+			// 
+			this.Delete.HeaderText = "";
+			this.Delete.Image = ((System.Drawing.Image)(resources.GetObject("Delete.Image")));
+			this.Delete.Name = "Delete";
+			// 
+			// ProductForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1129, 562);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.dgvUser);
+			this.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "ProductForm";
+			this.Text = "ProductForm";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.btnAdd)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dgvUser)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private CustomerButton btnAdd;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.DataGridView dgvUser;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+		private System.Windows.Forms.DataGridViewImageColumn Edit;
+		private System.Windows.Forms.DataGridViewImageColumn Delete;
+	}
+}
---
+++ Management System/ProductModuleForm.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Management_System
+{
+	partial class ProductModuleForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProductModuleForm));
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.pictureBoxClose = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txtRepass = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.btnClear = new System.Windows.Forms.Button();
+			this.btnUpdate = new System.Windows.Forms.Button();
+			this.btnSave = new System.Windows.Forms.Button();
+			this.label4 = new System.Windows.Forms.Label();
+			this.txtPass = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.txtFullName = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtUserName = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxClose)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.panel1.Controls.Add(this.pictureBoxClose);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(601, 40);
+			this.panel1.TabIndex = 14;
+			// 
+			// pictureBoxClose
+			// 
+			this.pictureBoxClose.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxClose.Image")));
+			this.pictureBoxClose.Location = new System.Drawing.Point(576, 0);
+			this.pictureBoxClose.Name = "pictureBoxClose";
+			this.pictureBoxClose.Size = new System.Drawing.Size(25, 22);
+			this.pictureBoxClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBoxClose.TabIndex = 9;
+			this.pictureBoxClose.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(131, 19);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Product Module";
+			// 
+			// txtRepass
+			// 
+			this.txtRepass.Location = new System.Drawing.Point(149, 235);
+			this.txtRepass.Name = "txtRepass";
+			this.txtRepass.Size = new System.Drawing.Size(329, 22);
+			this.txtRepass.TabIndex = 27;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(50, 240);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(76, 17);
+			this.label6.TabIndex = 26;
+			this.label6.Text = "Description";
+			// 
+			// btnClear
+			// 
+			this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(244)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.btnClear.FlatAppearance.BorderSize = 0;
+			this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnClear.ForeColor = System.Drawing.Color.White;
+			this.btnClear.Location = new System.Drawing.Point(402, 324);
+			this.btnClear.Name = "btnClear";
+			this.btnClear.Size = new System.Drawing.Size(75, 29);
+			this.btnClear.TabIndex = 25;
+			this.btnClear.Text = "Clear";
+			this.btnClear.UseVisualStyleBackColor = false;
+			// 
+			// btnUpdate
+			// 
+			this.btnUpdate.BackColor = System.Drawing.Color.Olive;
+			this.btnUpdate.FlatAppearance.BorderSize = 0;
+			this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnUpdate.ForeColor = System.Drawing.Color.White;
+			this.btnUpdate.Location = new System.Drawing.Point(306, 324);
+			this.btnUpdate.Name = "btnUpdate";
+			this.btnUpdate.Size = new System.Drawing.Size(75, 29);
+			this.btnUpdate.TabIndex = 24;
+			this.btnUpdate.Text = "Update";
+			this.btnUpdate.UseVisualStyleBackColor = false;
+			// 
+			// btnSave
+			// 
+			this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+			this.btnSave.FlatAppearance.BorderSize = 0;
+			this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.btnSave.ForeColor = System.Drawing.Color.White;
+			this.btnSave.Location = new System.Drawing.Point(210, 324);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(75, 29);
+			this.btnSave.TabIndex = 23;
+			this.btnSave.Text = "Save";
+			this.btnSave.UseVisualStyleBackColor = false;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(62, 277);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(64, 17);
+			this.label4.TabIndex = 21;
+			this.label4.Text = "Category";
+			// 
+			// txtPass
+			// 
+			this.txtPass.Location = new System.Drawing.Point(149, 198);
+			this.txtPass.Name = "txtPass";
+			this.txtPass.Size = new System.Drawing.Size(329, 22);
+			this.txtPass.TabIndex = 20;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(88, 203);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(38, 17);
+			this.label5.TabIndex = 19;
+			this.label5.Text = "Price";
+			// 
+			// txtFullName
+			// 
+			this.txtFullName.Location = new System.Drawing.Point(149, 161);
+			this.txtFullName.Name = "txtFullName";
+			this.txtFullName.Size = new System.Drawing.Size(329, 22);
+			this.txtFullName.TabIndex = 18;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(67, 166);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(59, 17);
+			this.label3.TabIndex = 17;
+			this.label3.Text = "Quantity";
+			// 
+			// txtUserName
+			// 
+			this.txtUserName.Location = new System.Drawing.Point(149, 124);
+			this.txtUserName.Name = "txtUserName";
+			this.txtUserName.Size = new System.Drawing.Size(329, 22);
+			this.txtUserName.TabIndex = 16;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(29, 129);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(97, 17);
+			this.label2.TabIndex = 15;
+			this.label2.Text = "Product Name:";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(149, 274);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(329, 25);
+			this.comboBox1.TabIndex = 10;
+			// 
+			// ProductModuleForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(601, 407);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.txtRepass);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.btnClear);
+			this.Controls.Add(this.btnUpdate);
+			this.Controls.Add(this.btnSave);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.txtPass);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.txtFullName);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.txtUserName);
+			this.Controls.Add(this.label2);
+			this.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "ProductModuleForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "ProductModuleForm";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxClose)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.PictureBox pictureBoxClose;
+		private System.Windows.Forms.Label label1;
+		public System.Windows.Forms.TextBox txtRepass;
+		private System.Windows.Forms.Label label6;
+		public System.Windows.Forms.Button btnClear;
+		public System.Windows.Forms.Button btnUpdate;
+		public System.Windows.Forms.Button btnSave;
+		private System.Windows.Forms.Label label4;
+		public System.Windows.Forms.TextBox txtPass;
+		private System.Windows.Forms.Label label5;
+		public System.Windows.Forms.TextBox txtFullName;
+		private System.Windows.Forms.Label label3;
+		public System.Windows.Forms.TextBox txtUserName;
+		private System.Windows.Forms.Label label2;
+		public System.Windows.Forms.ComboBox comboBox1;
+	}
+}
