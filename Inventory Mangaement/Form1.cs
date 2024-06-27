432326ccf2008de58c9e848a02f583c8ef5ace80
honam <75104291+honambsn@users.noreply.github.com>
2024-06-27 22:39:35 +0700

    add assets, modify location by code instead of drag&move

--- Inventory Mangaement/Form1.cs
+++ Inventory Mangaement/Form1.cs
@@ -15,10 +15,32 @@ namespace Inventory_Mangaement
 		public Form1()
 		{
 			InitializeComponent();
+			drawCenter();
+		}
+
+		private void drawCenter()
+		{
+			int panelWidth = panel1.Width;
+			int panelHeight = panel1.Height;
+
+			int xPos = (this.ClientSize.Width - panelWidth) / 2;
+			int yPos = (this.ClientSize.Height - panelHeight) / 2;
+
+			panel1.Location = new Point(xPos, yPos);
+
+			int labelWidth = label1.Width;
+			int labelHeight = label1.Height;
+
+			xPos = (this.panel1.Width - labelWidth) / 2;
+			yPos = (this.panel1.Height - labelHeight) / 2;
+
+			label1.Location = new Point(xPos, yPos);
+
 		}
 
 		private void label1_Click(object sender, EventArgs e)
 		{
+			
 
 		}
 
@@ -30,6 +52,21 @@ namespace Inventory_Mangaement
 		private void button4_Click(object sender, EventArgs e)
 		{
 
+		}
+
+		private void pictureBox3_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			this.Dispose();
+		}
+
+		private void panel1_Paint(object sender, PaintEventArgs e)
+		{
+			
 		}
 	}
 }
