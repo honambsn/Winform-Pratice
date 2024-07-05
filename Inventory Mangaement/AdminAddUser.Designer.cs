7bb11a3c43fed74e68fa0b976f795b9c8149cb04
honam <75104291+honambsn@users.noreply.github.com>
2024-07-05 23:05:43 +0700

    update: add adminadduser form

---
+++ Inventory Mangaement/AdminAddUser.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Inventory_Mangaement
+{
+	partial class AdminAddUser
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.label3 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.White;
+			this.panel1.Controls.Add(this.comboBox1);
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Controls.Add(this.textBox2);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.textBox1);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Location = new System.Drawing.Point(25, 22);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(330, 630);
+			this.panel1.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox2.Location = new System.Drawing.Point(15, 180);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(300, 29);
+			this.textBox2.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(15, 150);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(78, 20);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Password";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(15, 80);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(300, 29);
+			this.textBox1.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(15, 50);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(83, 20);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Username";
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.White;
+			this.panel2.Location = new System.Drawing.Point(375, 22);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(680, 630);
+			this.panel2.TabIndex = 1;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(15, 250);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(78, 20);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Password";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "Admin",
+            "Cashier"});
+			this.comboBox1.Location = new System.Drawing.Point(15, 280);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(300, 32);
+			this.comboBox1.TabIndex = 5;
+			// 
+			// AdminAddUser
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1080, 675);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "AdminAddUser";
+			this.Text = "AdminAddUser";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label3;
+	}
+}
--- Inventory Mangaement/Program.cs
+++ Inventory Mangaement/Program.cs
@@ -16,7 +16,7 @@ namespace Inventory_Mangaement
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+			Application.Run(new AdminAddUser());
 		}
 	}
 }
