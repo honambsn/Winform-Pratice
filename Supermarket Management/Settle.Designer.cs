c5acd3b4328211a8873cd02cfff6be6f5ccaa392
honam <75104291+honambsn@users.noreply.github.com>
2024-11-07 22:54:54 +0700

    update: settle payment

---
+++ Supermarket Management/Settle.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Supermarket_Management
+{
+	partial class Settle
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtSale = new System.Windows.Forms.TextBox();
+			this.txtCash = new System.Windows.Forms.TextBox();
+			this.txtChange = new System.Windows.Forms.TextBox();
+			this.btnSeven = new System.Windows.Forms.Button();
+			this.btnEight = new System.Windows.Forms.Button();
+			this.btnNine = new System.Windows.Forms.Button();
+			this.btnClear = new System.Windows.Forms.Button();
+			this.btnZero = new System.Windows.Forms.Button();
+			this.btnSix = new System.Windows.Forms.Button();
+			this.btnFive = new System.Windows.Forms.Button();
+			this.btnFour = new System.Windows.Forms.Button();
+			this.btnDZero = new System.Windows.Forms.Button();
+			this.btnThree = new System.Windows.Forms.Button();
+			this.btnTwo = new System.Windows.Forms.Button();
+			this.btnOne = new System.Windows.Forms.Button();
+			this.btnEnter = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtSale
+			// 
+			this.txtSale.Enabled = false;
+			this.txtSale.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtSale.Location = new System.Drawing.Point(14, 15);
+			this.txtSale.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txtSale.Multiline = true;
+			this.txtSale.Name = "txtSale";
+			this.txtSale.Size = new System.Drawing.Size(268, 35);
+			this.txtSale.TabIndex = 0;
+			// 
+			// txtCash
+			// 
+			this.txtCash.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtCash.Location = new System.Drawing.Point(14, 81);
+			this.txtCash.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txtCash.Multiline = true;
+			this.txtCash.Name = "txtCash";
+			this.txtCash.Size = new System.Drawing.Size(268, 35);
+			this.txtCash.TabIndex = 1;
+			this.txtCash.TextChanged += new System.EventHandler(this.txtCash_TextChanged);
+			// 
+			// txtChange
+			// 
+			this.txtChange.Enabled = false;
+			this.txtChange.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtChange.Location = new System.Drawing.Point(14, 147);
+			this.txtChange.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.txtChange.Multiline = true;
+			this.txtChange.Name = "txtChange";
+			this.txtChange.Size = new System.Drawing.Size(268, 35);
+			this.txtChange.TabIndex = 2;
+			// 
+			// btnSeven
+			// 
+			this.btnSeven.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(70)))), ((int)(((byte)(160)))));
+			this.btnSeven.ForeColor = System.Drawing.Color.White;
+			this.btnSeven.Location = new System.Drawing.Point(12, 202);
+			this.btnSeven.Name = "btnSeven";
+			this.btnSeven.Size = new System.Drawing.Size(50, 50);
+			this.btnSeven.TabIndex = 3;
+			this.btnSeven.Text = "7";
+			this.btnSeven.UseVisualStyleBackColor = false;
+			this.btnSeven.Click += new System.EventHandler(this.btnSeven_Click);
+			// 
+			// btnEight
+			// 
+			this.btnEight.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(70)))), ((int)(((byte)(160)))));
+			this.btnEight.ForeColor = System.Drawing.Color.White;
+			this.btnEight.Location = new System.Drawing.Point(85, 202);
+			this.btnEight.Name = "btnEight";
+			this.btnEight.Size = new System.Drawing.Size(50, 50);
+			this.btnEight.TabIndex = 4;
+			this.btnEight.Text = "8";
+			this.btnEight.UseVisualStyleBackColor = false;
+			this.btnEight.Click += new System.EventHandler(this.btnEight_Click);
+			// 
+			// btnNine
+			// 
+			this.btnNine.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(70)))), ((int)(((byte)(160)))));
+			this.btnNine.ForeColor = System.Drawing.Color.White;
+			this.btnNine.Location = new System.Drawing.Point(158, 202);
+			this.btnNine.Name = "btnNine";
+			this.btnNine.Size = new System.Drawing.Size(50, 50);
+			this.btnNine.TabIndex = 5;
+			this.btnNine.Text = "9";
+			this.btnNine.UseVisualStyleBackColor = false;
+			this.btnNine.Click += new System.EventHandler(this.btnNine_Click);
+			// 
+			// btnClear
+			// 
+			this.btnClear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(70)))), ((int)(((byte)(160)))));
+			this.btnClear.ForeColor = System.Drawing.Color.White;
+			this.btnClear.Location = new System.Drawing.Point(231, 202);
+			this.btnClear.Name = "btnClear";
+			this.btnClear.Size = new System.Drawing.Size(50, 50);
+			this.btnClear.TabIndex = 6;
+			this.btnClear.Text = "C";
+			this.btnClear.UseVisualStyleBackColor = false;
+			this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+			// 
+			// btnZero
+			// 
+			this.btnZero.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(70)))), ((int)(((byte)(160)))));
+			this.btnZero.ForeColor = System.Drawing.Color.White;
+			this.btnZero.Location = new System.Drawing.Point(231, 278);
+			this.btnZero.Name = "btnZero";
+			this.btnZero.Size = new System.Drawing.Size(50, 50);
+			this.btnZero.TabIndex = 10;
+			this.btnZero.Text = "0";
+			this.btnZero.UseVisualStyleBackColor = false;
+			this.btnZero.Click += new System.EventHandler(this.btnZero_Click);
+			// 
+			// btnSix
+			// 
+			this.btnSix.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(70)))), ((int)(((byte)(160)))));
+			this.btnSix.ForeColor = System.Drawing.Color.White;
+			this.btnSix.Location = new System.Drawing.Point(158, 278);
+			this.btnSix.Name = "btnSix";
+			this.btnSix.Size = new System.Drawing.Size(50, 50);
+			this.btnSix.TabIndex = 9;
+			this.btnSix.Text = "6";
+			this.btnSix.UseVisualStyleBackColor = false;
+			this.btnSix.Click += new System.EventHandler(this.btnSix_Click);
+			// 
+			// btnFive
+			// 
+			this.btnFive.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(70)))), ((int)(((byte)(160)))));
+			this.btnFive.ForeColor = System.Drawing.Color.White;
+			this.btnFive.Location = new System.Drawing.Point(85, 278);
+			this.btnFive.Name = "btnFive";
+			this.btnFive.Size = new System.Drawing.Size(50, 50);
+			this.btnFive.TabIndex = 8;
+			this.btnFive.Text = "5";
+			this.btnFive.UseVisualStyleBackColor = false;
+			this.btnFive.Click += new System.EventHandler(this.btnFive_Click);
+			// 
+			// btnFour
+			// 
+			this.btnFour.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(70)))), ((int)(((byte)(160)))));
+			this.btnFour.ForeColor = System.Drawing.Color.White;
+			this.btnFour.Location = new System.Drawing.Point(12, 278);
+			this.btnFour.Name = "btnFour";
+			this.btnFour.Size = new System.Drawing.Size(50, 50);
+			this.btnFour.TabIndex = 7;
+			this.btnFour.Text = "4";
+			this.btnFour.UseVisualStyleBackColor = false;
+			this.btnFour.Click += new System.EventHandler(this.btnFour_Click);
+			// 
+			// btnDZero
+			// 
+			this.btnDZero.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(70)))), ((int)(((byte)(160)))));
+			this.btnDZero.ForeColor = System.Drawing.Color.White;
+			this.btnDZero.Location = new System.Drawing.Point(231, 351);
+			this.btnDZero.Name = "btnDZero";
+			this.btnDZero.Size = new System.Drawing.Size(50, 50);
+			this.btnDZero.TabIndex = 14;
+			this.btnDZero.Text = "00";
+			this.btnDZero.UseVisualStyleBackColor = false;
+			this.btnDZero.Click += new System.EventHandler(this.btnDZero_Click);
+			// 
+			// btnThree
+			// 
+			this.btnThree.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(70)))), ((int)(((byte)(160)))));
+			this.btnThree.ForeColor = System.Drawing.Color.White;
+			this.btnThree.Location = new System.Drawing.Point(158, 351);
+			this.btnThree.Name = "btnThree";
+			this.btnThree.Size = new System.Drawing.Size(50, 50);
+			this.btnThree.TabIndex = 13;
+			this.btnThree.Text = "3";
+			this.btnThree.UseVisualStyleBackColor = false;
+			this.btnThree.Click += new System.EventHandler(this.btnThree_Click);
+			// 
+			// btnTwo
+			// 
+			this.btnTwo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(70)))), ((int)(((byte)(160)))));
+			this.btnTwo.ForeColor = System.Drawing.Color.White;
+			this.btnTwo.Location = new System.Drawing.Point(85, 351);
+			this.btnTwo.Name = "btnTwo";
+			this.btnTwo.Size = new System.Drawing.Size(50, 50);
+			this.btnTwo.TabIndex = 12;
+			this.btnTwo.Text = "2";
+			this.btnTwo.UseVisualStyleBackColor = false;
+			this.btnTwo.Click += new System.EventHandler(this.btnTwo_Click);
+			// 
+			// btnOne
+			// 
+			this.btnOne.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(70)))), ((int)(((byte)(160)))));
+			this.btnOne.ForeColor = System.Drawing.Color.White;
+			this.btnOne.Location = new System.Drawing.Point(12, 351);
+			this.btnOne.Name = "btnOne";
+			this.btnOne.Size = new System.Drawing.Size(50, 50);
+			this.btnOne.TabIndex = 11;
+			this.btnOne.Text = "1";
+			this.btnOne.UseVisualStyleBackColor = false;
+			this.btnOne.Click += new System.EventHandler(this.btnOne_Click);
+			// 
+			// btnEnter
+			// 
+			this.btnEnter.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(70)))), ((int)(((byte)(160)))));
+			this.btnEnter.ForeColor = System.Drawing.Color.White;
+			this.btnEnter.Location = new System.Drawing.Point(12, 428);
+			this.btnEnter.Name = "btnEnter";
+			this.btnEnter.Size = new System.Drawing.Size(269, 50);
+			this.btnEnter.TabIndex = 15;
+			this.btnEnter.Text = "Enter";
+			this.btnEnter.UseVisualStyleBackColor = false;
+			this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+			// 
+			// Settle
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(296, 490);
+			this.Controls.Add(this.btnEnter);
+			this.Controls.Add(this.btnDZero);
+			this.Controls.Add(this.btnThree);
+			this.Controls.Add(this.btnTwo);
+			this.Controls.Add(this.btnOne);
+			this.Controls.Add(this.btnZero);
+			this.Controls.Add(this.btnSix);
+			this.Controls.Add(this.btnFive);
+			this.Controls.Add(this.btnFour);
+			this.Controls.Add(this.btnClear);
+			this.Controls.Add(this.btnNine);
+			this.Controls.Add(this.btnEight);
+			this.Controls.Add(this.btnSeven);
+			this.Controls.Add(this.txtChange);
+			this.Controls.Add(this.txtCash);
+			this.Controls.Add(this.txtSale);
+			this.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.Name = "Settle";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Settle Payment";
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Settle_KeyDown);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtSale;
+		private System.Windows.Forms.TextBox txtCash;
+		private System.Windows.Forms.TextBox txtChange;
+		private System.Windows.Forms.Button btnSeven;
+		private System.Windows.Forms.Button btnEight;
+		private System.Windows.Forms.Button btnNine;
+		private System.Windows.Forms.Button btnClear;
+		private System.Windows.Forms.Button btnZero;
+		private System.Windows.Forms.Button btnSix;
+		private System.Windows.Forms.Button btnFive;
+		private System.Windows.Forms.Button btnFour;
+		private System.Windows.Forms.Button btnDZero;
+		private System.Windows.Forms.Button btnThree;
+		private System.Windows.Forms.Button btnTwo;
+		private System.Windows.Forms.Button btnOne;
+		private System.Windows.Forms.Button btnEnter;
+	}
+}
---
+++ Supermarket Management/Settle.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Supermarket_Management
+{
+	public partial class Settle : Form
+	{
+		SqlConnection cn = new SqlConnection();
+		SqlCommand cm = new SqlCommand();
+		DBConnect dbcon = new DBConnect();
+		SqlDataReader dr;
+		Cashier cashier;
+		public Settle(Cashier cash)
+		{
+			InitializeComponent();
+			cn = new SqlConnection(dbcon.myConnection());
+			this.KeyPreview = true;
+			cashier = cash;
+		}
+
+		private void btnOne_Click(object sender, EventArgs e)
+		{
+			txtCash.Text += btnOne.Text;
+		}
+
+		private void btnTwo_Click(object sender, EventArgs e)
+		{
+			txtCash.Text += btnTwo.Text;
+		}
+
+		private void btnThree_Click(object sender, EventArgs e)
+		{
+			txtCash.Text += btnThree.Text;
+		}
+
+		private void btnFour_Click(object sender, EventArgs e)
+		{
+			txtCash.Text += btnFour.Text;
+		}
+
+		private void btnFive_Click(object sender, EventArgs e)
+		{
+			txtCash.Text += btnFive.Text;
+		}
+
+		private void btnSix_Click(object sender, EventArgs e)
+		{
+			txtCash.Text += btnSix.Text;
+		}
+
+		private void btnSeven_Click(object sender, EventArgs e)
+		{
+			txtCash.Text += btnSeven.Text;
+		}
+
+		private void btnEight_Click(object sender, EventArgs e)
+		{
+			txtCash.Text += btnEight.Text;
+		}
+
+		private void btnNine_Click(object sender, EventArgs e)
+		{
+			txtCash.Text += btnNine.Text;
+		}
+
+		private void btnDZero_Click(object sender, EventArgs e)
+		{
+			txtCash.Text += btnDZero.Text;
+		}
+
+		private void btnZero_Click(object sender, EventArgs e)
+		{
+			txtCash.Text += btnZero.Text;
+		}
+
+		private void btnClear_Click(object sender, EventArgs e)
+		{
+			txtCash.Clear();
+			txtCash.Focus();
+		}
+
+		private void btnEnter_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if ((double.Parse(txtChange.Text) < 0) || (txtCash.Text.Equals("")))
+				{
+					MessageBox.Show("Insufficient amount, Please enter the correct amount!!!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+					return;
+				}
+				else
+				{
+					for (int i = 0; i < cashier.dgvCashier.Rows.Count; i++)
+					{
+						if (cn.State == ConnectionState.Open)
+						{
+							cn.Close();
+						}
+						cn.Open();
+
+						//using(SqlCommand cm = new SqlCommand("update Cart set status = 'Sold', cash = @cash, change = @change where id = @id", cn))
+						using (SqlCommand cm = new SqlCommand("update Product set qty = qty - " + int.Parse(cashier.dgvCashier.Rows[i].Cells[3].Value.ToString()) + " where pcode = '" + cashier.dgvCashier.Rows[i].Cells[5].Value.ToString()
+							+ "where ProductCode = @ProductCode '" + cashier.dgvCashier.Rows[i].Cells[2].Value.ToString() + "'", cn))
+						{
+							//cm.Parameters.AddWithValue("@cash", double.Parse(txtCash.Text));
+							//cm.Parameters.AddWithValue("@change", double.Parse(txtChange.Text));
+							//cm.Parameters.AddWithValue("@id", int.Parse(cashier.dgvCashier.Rows[i].Cells[0].Value.ToString()));
+							cm.ExecuteNonQuery();
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			finally
+			{
+
+			}
+		}
+
+		private void txtCash_TextChanged(object sender, EventArgs e)
+		{
+			try
+			{
+				double sale = 0;
+				double cash = 0;
+
+				
+				string saleText = txtSale.Text.Replace(',', '.');
+				string cashText = txtCash.Text.Replace(',', '.');
+
+				
+				if (!string.IsNullOrEmpty(saleText) && double.TryParse(saleText, out sale))
+				{
+					if (!string.IsNullOrEmpty(cashText) && double.TryParse(cashText, out cash))
+					{
+						double charge = cash - sale;
+						txtChange.Text = charge.ToString("#,##0.00");
+					}
+					else
+					{
+						txtChange.Text = "0.00";
+					}
+				}
+				else
+				{
+					txtChange.Text = "0.00";
+				}
+			}
+			catch (Exception ex)
+			{
+				txtChange.Text = "0.00";
+				MessageBox.Show(ex.Message);
+			}
+			finally
+			{
+
+			}
+		}
+
+		private void Settle_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Escape)
+			{
+				this.Dispose();
+			}
+			else if (e.KeyCode == Keys.Enter)
+			{
+				//btnEnter_Click(sender, e);
+				btnEnter.PerformClick();
+			}
+			else if (e.KeyCode == Keys.D1)
+			{
+				txtCash.Text += 1;
+			}
+			
+		}
+	}
+}
